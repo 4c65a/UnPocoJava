2b866d8e6c6b4b0e881f8cf9e2a78bac04bc1e67
Leandro Alexis Borquez <110344935+LeanBorquez@users.noreply.github.com>
2022-09-14 00:12:27 +0100

    Update Day3.java

--- src/Day3.java
+++ src/Day3.java
@@ -4,17 +4,9 @@ import java.io.InputStreamReader;
 
 class Result {
 
-    /*
-     * Complete the 'solve' function below.
-     *
-     * The function accepts following parameters:
-     *  1. DOUBLE meal_cost
-     *  2. INTEGER tip_percent
-     *  3. INTEGER tax_percent
-     */
-
+ 
     public static void solve(double meal_cost, int tip_percent, int tax_percent) {
-        // Write your code here
+    
     double tip = meal_cost*tip_percent/100;
     double tax = meal_cost*tax_percent/100;
     double total = meal_cost+tip+tax;
